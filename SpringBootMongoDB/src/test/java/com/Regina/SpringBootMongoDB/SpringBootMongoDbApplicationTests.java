80accbcbb200f8625ce217f0ce4d7731490eae51
ReginaNeto1995 <Reginaneto1995@outlook.pt>
2024-09-08 16:23:53 +0100

    Add files via upload

---
+++ SpringBootMongoDB/src/test/java/com/Regina/SpringBootMongoDB/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Regina.SpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
