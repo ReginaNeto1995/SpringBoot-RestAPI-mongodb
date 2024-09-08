f7ee90fa8669f6eb3e99bcbea652e3d95a8d574e
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2024-09-08 18:40:35 +0100

    Conexão ao MongoDB, userRepository, userService
    Conexão ao MongoDB, userRepository, userService, User resource Change

---
+++ SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Regina.SpringBootMongoDB.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Regina.SpringBootMongoDB.domain.User;
+
+@Repository
+public interface UserRepository  extends MongoRepository<User,String>{
+
+}
