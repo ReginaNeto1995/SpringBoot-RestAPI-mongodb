41dd37f724a3f9fd2e2127a1e4c452eafa90fd44
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2024-09-09 15:06:07 +0100

    Get User By Id, Exceptions
    Get User By Id, Exceptions , StandardError

--- SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/repository/UserRepository.java
+++ SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.Regina.SpringBootMongoDB.domain.User;
 
+
 @Repository
 public interface UserRepository  extends MongoRepository<User,String>{
 
--- SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/services/UserService.java
+++ SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/services/UserService.java
@@ -1,11 +1,16 @@
 package com.Regina.SpringBootMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.Regina.SpringBootMongoDB.domain.User;
 import com.Regina.SpringBootMongoDB.repository.UserRepository;
+import com.Regina.SpringBootMongoDB.services.exception.ObjectNotFoundException;
+
+
 
 @Service
 public class UserService {
@@ -18,5 +23,13 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id){
+		Optional<User> user = userRepository.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado !");
+		}
+		return user.get();
+	}
 	
+
 }
