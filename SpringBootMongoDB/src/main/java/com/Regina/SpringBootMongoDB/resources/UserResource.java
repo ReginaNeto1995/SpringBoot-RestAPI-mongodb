7413ba822b4fc657f69e48c6b4d90ae38b95f644
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2024-09-09 14:16:34 +0100

    UserDTO
    UserDTO

--- SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/resources/UserResource.java
+++ SpringBootMongoDB/src/main/java/com/Regina/SpringBootMongoDB/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.Regina.SpringBootMongoDB.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Regina.SpringBootMongoDB.domain.User;
+import com.Regina.SpringBootMongoDB.dto.UserDTO;
 import com.Regina.SpringBootMongoDB.services.UserService;
 
 @RestController
@@ -21,9 +23,18 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		/**
+		 * No controller vamos retornar uma DTO, para isso:
+		 * 1 º - criamos a lista para guardar os objetos DTO
+		 * 2 º - Vamos converter a lista de Users original para uma DTO, para isso vamos usar uma expressão lambda
+		 * 3 º - list.stream() para transformar a lista numa stream (coleção compativel com as expressões lambda)
+		 * 4 º - list.stram().map(x -> new UserDTO(x) o map vai pegar cada elemento da lista de User (x) e transformar no tipo UserDTO
+		 * 5 º - collect(Collectors.toList()) -> Por fim volto a tranformar a stream numa lista
+		 */	
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 		
 	}
 
